3d9454d9efa66b4bc018d7fd0dcaac22455074eb
Kirsten Hipolito <kirsten.hipolito@eee.upd.edu.ph>
2020-05-28 05:06:57 +0100

    update

--- inference_cpp/src/inference_vocp_standardized_larq.cpp
+++ inference_cpp/src/inference_vocp_standardized_larq.cpp
@@ -1,18 +1,18 @@
 #include <cstdio>
 #include <ctime>
 #include <iostream>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/core/matx.hpp>
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-#include <ctime>
+#include <iomanip>
 #include <cmath>
 #include <string>
 #include <iterator>
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/core/matx.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 
 #include "decode_detections.hpp"
 #include "larq_compute_engine/tflite/kernels/lce_ops_register.h"
@@ -89,18 +89,23 @@ std::istream& operator>>(std::istream& str, CSVRow& data)
 }  
 
 int main(int argc, char* argv[]) {
-    if (argc != 1) {
-        fprintf(stderr, "minimal <tflite model>\n");
+    if (argc != 2) {
+        fprintf(stderr, "./bin/inference_trial <tflite model>\n");
         return 1;
     }
-    const char* filename = "../../models_trained/ssd_mobilenet_larq.tflite";
+    // const char* filename = "../../models_trained/ssd_mobilenet_tflite.tflite";
+    char* filename = argv[1];
+
+    int num_runs = 25;
 
     cv::Mat image;
     cv::Mat resized;
     int image_height = 300;
     int image_width = 300;
     int image_channels = 3;
-    clock_t time_req, time_req_1, time_req_2;
+    clock_t time_req_1, time_req_2;
+    clock_t ave_invoke_ms = 0;
+    clock_t ave_inference_ms = 0;
 
     string img_directory = "../../datasets/VOCdevkit_test/VOC2007/JPEGImages/";
     string img_csv = "../../ssd-keras/dataset_voc_csv/2007_person_test.csv";
@@ -137,52 +142,51 @@ int main(int argc, char* argv[]) {
     // printf("=== Pre-invoke Interpreter State ===\n");
     // tflite::PrintInterpreterState(interpreter.get());
 
-    int i = 0;
+    file >> row;
 
-    while(file >> row)
-    {
+    std::cout << "   Image    | im+inf(ms) |  inf(ms)   | im+inf(fps)|  inf(fps) " << std::endl;
+    std::cout << std::fixed;
+    std::cout << std::setprecision(6);
+
+    for (int i = 0; i < num_runs; i++) {
+        file >> row;
         img_path = img_directory + row[0];
-        std::cout << img_path << std::endl;
+        // std::cout << img_path.c_str() << std::endl;
 
-        time_req_1 = clock();
+        time_req_1 = clock(); //time_req_1 -> ave_inference_ms
 
-        // Fill input buffers
-        // resize image and load into input  
-        image = cv::Mat();
-        image = imread(img_path, cv::IMREAD_COLOR);
-        
+        // Fill input buffers, resize image and load into input  
+        image = cv::imread(img_path, cv::IMREAD_COLOR);
         if (image.empty()) {
             std::cout << "Could not read the image: " << img_path << std::endl;
-            continue;
-        }
-        
+        } 
         cv::resize(image, resized, cv::Size(image_width,image_height));
-        fill_buffer_with_mat(resized,to_inp,image_height,image_width,image_channels);
-        
-        time_req_2 = clock();
+        // memcpy(interpreter->typed_input_tensor<float>(0), image.data, image.total() * image.elemSize());
+        fill_buffer_with_mat(resized,interpreter->typed_input_tensor<float>(0),image_height,image_width,image_channels);
+        time_req_2 = clock(); //time_req_1 -> ave_invoke_ms
 
         // Run inference
         TFLITE_MINIMAL_CHECK(interpreter->Invoke() == kTfLiteOk);
         // printf("\n\n=== Post-invoke Interpreter State ===\n");
         // tflite::PrintInterpreterState(interpreter.get());
-        std::cout << "Done invoking for image " << ++i << std::endl;
 
         float* output = interpreter->typed_output_tensor<float>(0);
 
         time_req_1 = clock() - time_req_1;
         time_req_2 = clock() - time_req_2;
 
-        cout << "Loading image and inference of model took " << (float)time_req_1/CLOCKS_PER_SEC << " seconds, or equivalent FPS of " << CLOCKS_PER_SEC/(float)time_req_1 << endl;
-        cout << "Only inference of model took " << (float)time_req_2/CLOCKS_PER_SEC << " seconds, or equivalent FPS of " << CLOCKS_PER_SEC/(float)time_req_2 << endl;
-        
+        // cout << "Loading image and inference of model took " << (float)time_req_1/CLOCKS_PER_SEC << " seconds, or equivalent FPS of " << CLOCKS_PER_SEC/(float)time_req_1 << endl;
+        // cout << "Only inference of model took " << (float)time_req_2/CLOCKS_PER_SEC << " seconds, or equivalent FPS of " << CLOCKS_PER_SEC/(float)time_req_2 << endl;
+        std::cout << row[0] << "  |  " << (float)time_req_1*1000/CLOCKS_PER_SEC << "  |  " << (float)time_req_2*1000/CLOCKS_PER_SEC << "  |  " << CLOCKS_PER_SEC/(float)time_req_1 << "  |  " << CLOCKS_PER_SEC/(float)time_req_2 << std::endl;
         // Read output buffers
         // TODO(user): Insert getting data out code.
-        
-        if (i > 25) {
-            break;
-        }
+        ave_inference_ms += time_req_1;
+        ave_invoke_ms += time_req_2;
+
     }
-    
+
+    std::cout << "Average invoke time (ms): " << (float)ave_invoke_ms*1000/(CLOCKS_PER_SEC*num_runs) << std::endl;
+    std::cout << "Average inference time (ms): " << (float)ave_inference_ms*1000/(CLOCKS_PER_SEC*num_runs) << std::endl;
 
     return 0;
 }
