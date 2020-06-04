e6a969eda161a483752e1292c0fae7920d6644f9
Kirsten Hipolito <kirsten.hipolito@eee.upd.edu.ph>
2020-06-05 03:16:03 +0800

    updated threading and timing for inference pipeline

--- inference_cpp/src/inference_dataset.cpp
+++ inference_cpp/src/inference_dataset.cpp
@@ -97,8 +97,9 @@ int main(int argc, char* argv[]) {
     char* filename = argv[1];
 
     int num_runs = 50;
-    clock_t ave_invoke_ms = 0;
-    clock_t ave_inference_ms = 0;
+    float ave_invoke_ms = 0;
+    float ave_inference_ms = 0;
+    int num_threads = 4;
 
     cv::Mat image;
     cv::Mat resized;
@@ -127,9 +128,8 @@ int main(int argc, char* argv[]) {
     compute_engine::tflite::RegisterLCECustomOps(&resolver);
 
     // Build the interpreter
-    tflite::InterpreterBuilder builder(*model, resolver);
     std::unique_ptr<Interpreter> interpreter;
-    builder(&interpreter);
+    tflite::InterpreterBuilder(*model, resolver)(&interpreter,num_threads);
     TFLITE_MINIMAL_CHECK(interpreter != nullptr);
 
     interpreter->SetNumThreads(4);
@@ -145,7 +145,6 @@ int main(int argc, char* argv[]) {
 
     file >> row;
 
-    std::cout << "   Image    | im+inf(ms) |  inf(ms)   | im+inf(fps)|  inf(fps) " << std::endl;
     std::cout << std::fixed;
     std::cout << std::setprecision(6);
 
@@ -153,7 +152,7 @@ int main(int argc, char* argv[]) {
         file >> row;
         img_path = img_directory + row[0];
 
-        time_req_1 = clock(); //time_req_1 -> ave_inference_ms
+        auto start1 = std::chrono::steady_clock::now();
 
         // Fill input buffers, resize image and load into input  
         image = cv::imread(img_path, cv::IMREAD_COLOR);
@@ -164,27 +163,24 @@ int main(int argc, char* argv[]) {
         // memcpy(interpreter->typed_input_tensor<float>(0), image.data, image.total() * image.elemSize());
         fill_buffer_with_mat(resized,interpreter->typed_input_tensor<float>(0),image_height,image_width,image_channels);
         
-        time_req_2 = clock(); //time_req_1 -> ave_invoke_ms
+        auto start2 = std::chrono::steady_clock::now();
 
         // Run inference
         TFLITE_MINIMAL_CHECK(interpreter->Invoke() == kTfLiteOk);
         float* output = interpreter->typed_output_tensor<float>(0);
 
-        time_req_1 = clock() - time_req_1;
-        time_req_2 = clock() - time_req_2;
+        // Get end clock
+        auto end = std::chrono::steady_clock::now();
 
-        std::cout << row[0] << "  |  " << (float)time_req_1*1000/CLOCKS_PER_SEC << "  |  " << (float)time_req_2*1000/CLOCKS_PER_SEC << "  |  " << CLOCKS_PER_SEC/(float)time_req_1 << "  |  " << CLOCKS_PER_SEC/(float)time_req_2 << std::endl;
-        
-        // Read output buffers
-        // TODO(user): Insert getting data out code.
-        
-        ave_inference_ms += time_req_1;
-        ave_invoke_ms += time_req_2;
+        std::cout << "Time of invoke (ms): " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start2).count() << std::endl;
+        std::cout << "Time of inference (ms): " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start1).count() << std::endl;
+        ave_invoke_ms += std::chrono::duration_cast<std::chrono::milliseconds>(end - start2).count();
+        ave_inference_ms += std::chrono::duration_cast<std::chrono::milliseconds>(end - start1).count();
 
     }
 
-    std::cout << "Average invoke time (ms): " << (float)ave_invoke_ms*1000/(CLOCKS_PER_SEC*num_runs) << std::endl;
-    std::cout << "Average inference time (ms): " << (float)ave_inference_ms*1000/(CLOCKS_PER_SEC*num_runs) << std::endl;
+    std::cout << "Average invoke time (ms): " << (float)ave_invoke_ms/num_runs << std::endl;
+    std::cout << "Average inference time (ms): " << (float)ave_inference_ms/num_runs << std::endl;
 
     return 0;
 }
